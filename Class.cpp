1f03242f237506ae92e8721ab5d9c81842e3a6ec
Kelvin <Kelvinfriedchicken@gmail.com>
2021-11-25 06:08:26 +0000

    Initial 'template' commit

---
+++ Class.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+//Classes
+//Step 1
+//Data expected to see inside a footabll stadiums
+//Football Players Vairables
+	/*
+	Player name	//str
+	Prefered Foot	//Bool
+	Height	//Float
+	Weight	//Int
+	Age	//Int
+	Speed	//Float
+	Ball Control	//Int
+	Shooting	//int
+	Tackaling	//Int
+	Positioning	//int
+	Fitness	int
+	*/
+//Footbal Team Vairables
+	/*
+	Team Name	//str
+	Team Colour	//str
+	Manager	//str
+	
+	Players	//Football playrs
+	Players on pitch	//int
+	Players on bench	//int
+	Num of Red cards	//int
+	Num of Yellow cards	//int
+	*/
+//Football Stadium Vairables
+	/*
+	Stadium Name	//str
+	Stadium Location	//str
+	Football Teams	//Football team
+	
+	Location of Ball	// float[2]
+	num of supporters	//int
+	Time Left in game	//float
+	*/
+	
