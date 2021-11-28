a6009b7c5f060f0f62119d0e3888a1a9cf7ee991
Kelvin <Kelvinfriedchicken@gmail.com>
2021-11-28 07:50:21 +0000

    Moved class definitions into the main function

--- Class.cpp
+++ Class.cpp
@@ -40,218 +40,217 @@
 	num of supporters	//int
 	Time Left in game	//float
 	*/
-
-class Football_Player
-{
-	public:
-		std::string Name;
-		bool Prefered_Foot;	//Left is 0, Right is 1.
-		float Height;	//In Meters
-		int Weight;	//In KG
-		int Age;
-		int Number;
-		float Speed;	//M/S
-		int Ball_Control;	//Generic Value, how well they can use the ball
-		int Shooting;	//Genetic value, how well they can kick a ball
-		int Tackling;	//How well they can tackle a ball
-		int Positioning;	//How well the players position themself 
-		int Fitness;	//How much stamina the player has.
-	
-		Football_Player()
-		{
-			Name = "Default Player Name";
-			Height = 1.5;
-			Weight = 50;
-			Age = 18;
-			Number = 0;
-			Speed = 5;
-			Ball_Control = 10;
-			Shooting = 5;
-			Tackling = 5;
-			Positioning = 5;
-			Fitness = 5;
-		}
-};
-
-class Football_Team
-{
-	public:
-		std::string Name;
-		std::string Colour;
-		std::string Manager;
-		Football_Player Players[15];
-		std::string Players_on_pitch[11];
-		std::string Players_on_bench[4];
-		int Yellow_cards;
-		int Red_cards;
-	
-		Football_Team()
-		{
-			const short ELEVEN = 11;
-			const short FOUR = 4;
-			
-			Name = "Default Team Name";
-			Colour = "Black";
-			Manager = "Default Manager Name";
-			//Player passed as it already has it's default values.
-			for( int x = 0; x < ELEVEN; x++ )	//Trying to see if this works.
-			{
-				//Players_on_bench[ x ] = Players.Name();
-				//std::cout<< "Test" << std::endl; _/ Test passed
-				Players_on_pitch[ x ] = "Default Player Name";
-			}
-			
-			for( int x = 0; x < FOUR; x++ )
-			{
-				//Players_on_bench[ x + 10 ] = Players.Name();
-				//std::cout<< "Test" << std::endl;	//Only happens twice
-				Players_on_bench[ x ] = "Default Player Name";
-				//std::cout<< "Done" << std::endl;	// apperas to end after here on two turns
-			} 
-			//An object needs to be created inorder to referance it's attributes; So in theroy this should be done at run time of the game itself
-			
-			int Yellow_cards = 0;
-			int Red_cards = 0;
-		}
-};
-
-class Football_Stadium
-{
-	public:
-		std::string Name;
-		std::string Location;
-		Football_Team Teams[2];
-		float Location_of_ball;
-		int Supporters;
-		float Time_left;
-	
-		Football_Stadium()
-		{
-			const short TWO = 2;
-			
-			Name = "Default Stadium Name";
-			Location = "Default Stadium Location";
-			/*for( int x = 0; x < TWO; x++)
-			{
-				Teams[ x ] = "Default Team Name";
-			}*/ //Lost track of what I was doing, This is dumb as I was already set the team name in the football team class. My bad
-			
-			Location_of_ball = 0.0f;
-			Supporters = 0;
-			Time_left = 90.0f;
-		}
-};
-
-class RPG_Item	//I want the enimies to have the items on their bodies
-{
-	public:
-		std::string Name;
-		int Value;
-		int Weight;
-		bool Weapon;
-		int Damage;
-		
-		RPG_Item()
-		{
-			Name = "Default Item Name";
-			Value = 0;
-			Weight = 0;
-			Weapon = false;
-			Damage = 0;
-		}
-};
-
-class RPG_Inventory_Slot
-{
-	public:
-		RPG_Item Item;
-		int Slot_number;
-		
-		RPG_Inventory_Slot()
-		{
-			Slot_number = 99;
-		}
-};
-
-class RPG_Character
-{
-	public:
-		std::string Name;
-		int Height;
-		int Mana;
-		RPG_Inventory_Slot Slot[ 2 ];
-			short Equipt_Weapon;
-		
-		int Start_location[ 2 ];
-		int Current_location[ 2 ];
-		
-		RPG_Character()
-		{
-			Name = "Default Character Name";
-			Height = 1;
-			Mana =  0;
-			/*for( int x = 0; x < 10; x++ )
-			{
-				Inventory_Slots[ x ] = "No Item in this slot";
-			}	//Need to fill array with a null value;
-			for( int x = 0; x < 2; x++ )
-			{
-				//Inventory_Slots[ x ] = '\0';	//Testing to see if a null character works	//Does not work
-			}*/
-			
-			Equipt_Weapon = 0;
-			
-			for( int x = 0; x < 2; x++ )
-			{
-				Start_location[ x ] = 0;
-				Current_location[ x ] = 0;
-			}
-		}
-		
-		void Attack_check()
-		{
-			/*if( Slot[ Equipt_Weapon ].Item.Weapon == true )
-			{
-				std::cout<< "You can attack with this weapon" << std::endl;
-			}
-			
-			else
-			{
-				std::cout<< "You cannot attack with this weapon" << std::endl;
-			}*/
-			std::cout<< " The slot is " << Slot[ 0 ].Slot_number << std::endl;
-		}
-};
-
-class RPG_Dungeon
-{
-	public:
-		std::string Name;
-		RPG_Character Player;
-		RPG_Character Entity[ 4 ];	//Need to add a start location to this so it's not redundant
-		
-		/*int Location_Of_Player[ 2 ];
-		int Start_location[ 2 ];
-		int End_location[ 2 ];*/	//Not needed as these will be kept in the players class rather than in the dungeon
-		
-		int Entry_Point[ 2 ];
-		int Exit_Point[ 2 ];
-		
-		RPG_Dungeon()
-		{
-			Name = "Default Dungeon Name";
-			/*Entry_Point = { 0, 0 };
-			Exit_Point = { 0, 0 };*/
-			for( int x = 0; x < 2; x++ )
-			{
-				Entry_Point[ x ] = 0;
-				Exit_Point[ x ] = 0;
-			}
-		}
-};
-
 int main()
 {
+	
+	class Football_Player
+	{
+		public:
+			std::string Name;
+			bool Prefered_Foot;	//Left is 0, Right is 1.
+			float Height;	//In Meters
+			int Weight;	//In KG
+			int Age;
+			int Number;
+			float Speed;	//M/S
+			int Ball_Control;	//Generic Value, how well they can use the ball
+			int Shooting;	//Genetic value, how well they can kick a ball
+			int Tackling;	//How well they can tackle a ball
+			int Positioning;	//How well the players position themself 
+			int Fitness;	//How much stamina the player has.
+		
+			Football_Player()
+			{
+				Name = "Default Player Name";
+				Height = 1.5;
+				Weight = 50;
+				Age = 18;
+				Number = 0;
+				Speed = 5;
+				Ball_Control = 10;
+				Shooting = 5;
+				Tackling = 5;
+				Positioning = 5;
+				Fitness = 5;
+			}
+	};
+
+	class Football_Team
+	{
+		public:
+			std::string Name;
+			std::string Colour;
+			std::string Manager;
+			Football_Player Players[15];
+			std::string Players_on_pitch[11];
+			std::string Players_on_bench[4];
+			int Yellow_cards;
+			int Red_cards;
+		
+			Football_Team()
+			{
+				const short ELEVEN = 11;
+				const short FOUR = 4;
+				
+				Name = "Default Team Name";
+				Colour = "Black";
+				Manager = "Default Manager Name";
+				//Player passed as it already has it's default values.
+				for( int x = 0; x < ELEVEN; x++ )	//Trying to see if this works.
+				{
+					//Players_on_bench[ x ] = Players.Name();
+					//std::cout<< "Test" << std::endl; _/ Test passed
+					Players_on_pitch[ x ] = "Default Player Name";
+				}
+				
+				for( int x = 0; x < FOUR; x++ )
+				{
+					//Players_on_bench[ x + 10 ] = Players.Name();
+					//std::cout<< "Test" << std::endl;	//Only happens twice
+					Players_on_bench[ x ] = "Default Player Name";
+					//std::cout<< "Done" << std::endl;	// apperas to end after here on two turns
+				} 
+				//An object needs to be created inorder to referance it's attributes; So in theroy this should be done at run time of the game itself
+				
+				int Yellow_cards = 0;
+				int Red_cards = 0;
+			}
+	};
+
+	class Football_Stadium
+	{
+		public:
+			std::string Name;
+			std::string Location;
+			Football_Team Teams[2];
+			float Location_of_ball;
+			int Supporters;
+			float Time_left;
+		
+			Football_Stadium()
+			{
+				const short TWO = 2;
+				
+				Name = "Default Stadium Name";
+				Location = "Default Stadium Location";
+				/*for( int x = 0; x < TWO; x++)
+				{
+					Teams[ x ] = "Default Team Name";
+				}*/ //Lost track of what I was doing, This is dumb as I was already set the team name in the football team class. My bad
+				
+				Location_of_ball = 0.0f;
+				Supporters = 0;
+				Time_left = 90.0f;
+			}
+	};
+
+	class RPG_Item	//I want the enimies to have the items on their bodies
+	{
+		public:
+			std::string Name;
+			int Value;
+			int Weight;
+			bool Weapon;
+			int Damage;
+			
+			RPG_Item()
+			{
+				Name = "Default Item Name";
+				Value = 0;
+				Weight = 0;
+				Weapon = false;
+				Damage = 0;
+			}
+	};
+
+	class RPG_Inventory_Slot
+	{
+		public:
+			RPG_Item Item;
+			int Slot_number;
+			
+			RPG_Inventory_Slot()
+			{
+				Slot_number = 99;
+			}
+	};
+
+	class RPG_Character
+	{
+		public:
+			std::string Name;
+			int Height;
+			int Mana;
+			RPG_Inventory_Slot Slot[ 2 ];
+				short Equipt_Weapon;
+			
+			int Start_location[ 2 ];
+			int Current_location[ 2 ];
+			
+			RPG_Character()
+			{
+				Name = "Default Character Name";
+				Height = 1;
+				Mana =  0;
+				/*for( int x = 0; x < 10; x++ )
+				{
+					Inventory_Slots[ x ] = "No Item in this slot";
+				}	//Need to fill array with a null value;
+				for( int x = 0; x < 2; x++ )
+				{
+					//Inventory_Slots[ x ] = '\0';	//Testing to see if a null character works	//Does not work
+				}*/
+				
+				Equipt_Weapon = 0;
+				
+				for( int x = 0; x < 2; x++ )
+				{
+					Start_location[ x ] = 0;
+					Current_location[ x ] = 0;
+				}
+			}
+			
+			void Attack_check()
+			{
+				/*if( Slot[ Equipt_Weapon ].Item.Weapon == true )
+				{
+					std::cout<< "You can attack with this weapon" << std::endl;
+				}
+				
+				else
+				{
+					std::cout<< "You cannot attack with this weapon" << std::endl;
+				}*/
+				std::cout<< " The slot is " << Slot[ 0 ].Slot_number << std::endl;
+			}
+	};
+
+	class RPG_Dungeon
+	{
+		public:
+			std::string Name;
+			RPG_Character Player;
+			RPG_Character Entity[ 4 ];	//Need to add a start location to this so it's not redundant
+			
+			/*int Location_Of_Player[ 2 ];
+			int Start_location[ 2 ];
+			int End_location[ 2 ];*/	//Not needed as these will be kept in the players class rather than in the dungeon
+			
+			int Entry_Point[ 2 ];
+			int Exit_Point[ 2 ];
+			
+			RPG_Dungeon()
+			{
+				Name = "Default Dungeon Name";
+				/*Entry_Point = { 0, 0 };
+				Exit_Point = { 0, 0 };*/
+				for( int x = 0; x < 2; x++ )
+				{
+					Entry_Point[ x ] = 0;
+					Exit_Point[ x ] = 0;
+				}
+			}
+	};
 	//Football Code
 	/*Football_Player player_a; //Initialised an object from the class 
 	
@@ -278,8 +277,36 @@ int main()
 	
 	std::cout<<"You the player are called  " << Starting_Dungeon.Player.Name << " you have a height of " << Starting_Dungeon.Player.Height << std::endl;
 	
-	//std::cout<<"You are jumped and attempt to attack.. " << Starting_Dungeon.Player.Attack_check() << std::endl;	//Testing using the attack method	//Still cannot get this to work
+	std::cout<<"You are jumped and attempt to attack.. " << Starting_Dungeon.Player.Attack_check() << std::endl;	//Testing using the attack method	//Still cannot get this to work
 	//std::cout<<"You are jumped and attempt to attack.. " << Starting_Dungeon.Player.Slot[0].Item.Weapon << std::endl;	//Testing using the attack method
 	
 	return 0;
 }
+
+class Class_1
+{
+	public:
+		int Value;
+		
+		Class_1()
+		{
+			Value = 1;
+		}
+};
+
+class Class_2
+{
+	public:
+		Class_1 Child_class;
+		int Value;
+		
+		Class_2()
+		{
+				Value = 2;
+		}
+		
+		Value_compare()
+		{
+			std::cout<< "The value from this class is:" << Value << " The child class has a value of " << Child_class.Value << std::endl; 
+		}
+};
